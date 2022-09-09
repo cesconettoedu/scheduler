10b6db0a0261b8dad392526a619d1ce707f2cdb7
Eduardo <cesconettoedu@gmail.com>
2022-09-09 14:11:48 -0400

    props and state in the appointment form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -24,7 +24,8 @@ export default function Form (props) {
   return ( 
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+                                 {/* to prevent the error pressing enter */}
+        <form autoComplete="off" onSubmit={event => { event.preventDefault(); }}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
@@ -41,12 +42,13 @@ export default function Form (props) {
           interviewers={props.interviewers}
           value={interviewer}
           onChange={setInterviewer}
+          
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={props.onSave} >Save</Button>
+          <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
     </main>
