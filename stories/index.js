55dd323e3767fac81da8b0f1689e867427791980
Eduardo <cesconettoedu@gmail.com>
2022-09-09 07:49:56 -0400

    add error alert

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Empty from "components/Appointment/Empty.js";
 import Show from "components/Appointment/Show.js";
 import Confirm from "components/Appointment/Confirm.js";
 import Status from "components/Appointment/Status.js";
+import Error from "components/Appointment/Error.js";
 
 
 storiesOf("Button", module)
@@ -178,3 +179,8 @@ storiesOf("Appointment", module)
                       />)
   
   .add("Status", () => <Status message={"Deleting"} />)
+
+  .add("Error", () => <Error 
+                        message={"Could not delete appointment."}
+                        onClose={action("onClose")}
+                      />)
