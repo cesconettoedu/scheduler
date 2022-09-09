e729592d07f6851bd77200642ad96d69aa1e539c
Eduardo <cesconettoedu@gmail.com>
2022-09-09 15:13:27 -0400

    displaying appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,14 +1,34 @@
 import React from "react";
 import "./styles.scss";
+import { Fragment } from 'react'
+
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
+
 
 export default function Appointment (props) {
 
   return (
+      <article className="appointment">
 
-    <article className="appointment">
+          <Header time={props.time} />
 
-      
-    </article>
+          <Fragment>
+    
+            {props.interview? 
+              <Show 
+                  student={props.interview.student}
+                  interviewer={props.interview.interviewer}
+              /> :
+              <Empty />
+            }
+          
+          </Fragment>
 
+      </article>
   );
 }
+
+
