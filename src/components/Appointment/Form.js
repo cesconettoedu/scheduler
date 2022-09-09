98a1cbcead884eafef57607733ea5ec215a2c561
Eduardo <cesconettoedu@gmail.com>
2022-09-09 13:33:35 -0400

    update buttons action Cancel and save

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -9,6 +9,18 @@ export default function Form (props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
    
+  //reset inputs
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null); 
+  };
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
+
   return ( 
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -18,20 +30,23 @@ export default function Form (props) {
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            /*
-              This must be a controlled component
-              your code goes here
-            */
+           
+            onChange={event => {
+              setStudent(event.target.value);
+            }}
+
           />
         </form>
         <InterviewerList 
-          /* your code goes here */
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
-          <Button confirm onClick={props.onSave}>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={props.onSave} >Save</Button>
         </section>
       </section>
     </main>
