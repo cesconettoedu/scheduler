ab4a5bc3296c3df6311f2eeec3ab2521534618e7
Eduardo <cesconettoedu@gmail.com>
2022-09-28 16:15:02 -0400

    finish Easier Async

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -7,11 +7,23 @@ import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
+// it("defaults to Monday and changes the schedule when a new day is selected", () => {
+//   const { getByText } = render(<Application />);
+
+//   return waitForElement(() => getByText("Monday")).then(() => {
+//     fireEvent.click(getByText("Tuesday"));
+    
+//     expect(getByText("Leopold Silvers")).toBeInTheDocument();
+//   });
+// });
+
+//new sintax  ECMAScript 2017 using async
+it("changes the schedule when a new day is selected", async () => {
   const { getByText } = render(<Application />);
 
-  return waitForElement(() => getByText("Monday")).then(() => {
-    fireEvent.click(getByText("Tuesday"));
-    expect(getByText("Leopold Silvers")).toBeInTheDocument();
-  });
+  await waitForElement(() => getByText("Monday"));
+
+  fireEvent.click(getByText("Tuesday"));
+
+  expect(getByText("Leopold Silvers")).toBeInTheDocument();
 });
